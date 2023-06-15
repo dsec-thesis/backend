a74cdda61966b0dca7f66ef7984c28d0d070c831
emanuelcima <emanuel.cima@hotmail.com>
2023-06-15 14:16:33 -0300

    fix format

--- backend/apps/api/main.py
+++ backend/apps/api/main.py
@@ -8,7 +8,7 @@ from backend.apps.container import Container
 
 app = FastAPI()
 container = Container()
-app.container = container # type: ignore
+app.container = container  # type: ignore
 app.include_router(bookings.router, prefix="/bookings", tags=["bookings"])
 app.include_router(parkinglots.router, prefix="/parkinglots", tags=["parkinglots"])
 app.include_router(searcher.router, prefix="/searcher", tags=["searcher"])
